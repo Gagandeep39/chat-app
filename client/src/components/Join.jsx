ce82380f52fc2fcdd09881479f38e59ec0f8b3c8
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-20 17:35:26 +0000

    Created a simple form

--- client/src/components/Join.jsx
+++ client/src/components/Join.jsx
@@ -5,10 +5,44 @@
  * @modify date 2021-02-20 14:27:08
  * @desc [description]
  */
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Join = () => {
-  return <div>Join Component</div>;
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            placeholder="Name"
+            className="joinInput"
+            type="text"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Room"
+            className="joinInput mt-20"
+            type="text"
+            onChange={(event) => setRoom(event.target.value)}
+          />
+        </div>
+        {/* onClick ensures button will redirect only if both values are not null */}
+        <Link
+          to={`chat?name=${name}&room=${room}`}
+          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
+        >
+          <button className="button mt-20" type="submit">
+            Sign In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Join;
