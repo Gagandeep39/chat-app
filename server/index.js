d197aa19f9350bb01ee3819ec3dd7da988d69d79
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-20 18:00:22 +0000

    Added server for soket IO with CORS

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@
  * @desc [description]
  */
 const express = require('express');
-const socketio = require('socket.io');
+
 const http = require('http');
 const router = require('./router');
 const PORT = process.env.PORT || 5000;
@@ -14,7 +14,20 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 const server = http.createServer(app);
-const io = socketio(server);
-app.use(router);
 
+// Socket Config
+const socketio = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }
+});
+socketio.on('connection', (socket) => {
+  console.log('New Connection Created~');
+
+  socket.on('disconnect', () => {
+    console.log('User isconnected');
+  });
+});
+
+app.use(router);
 server.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
