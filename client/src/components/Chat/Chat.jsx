a26b1fde1c96670bf6e506c87a18c5ddae220a6c
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-20 18:00:41 +0000

    Added chat component logic

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -5,11 +5,25 @@
  * @modify date 2021-02-20 14:27:11
  * @desc [description]
  */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string'; // Parses query Params
+import io from 'socket.io-client';
 
 import './Chat.css';
 
-const Chat = () => {
+let socket;
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const applicationUrl = process.env.REACT_APP_SERVER_URL || 'localhost:5000';
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    socket = io(applicationUrl);
+    setName(name);
+    setRoom(room);
+    console.log(socket);
+  }, [location]);
+
   return <div>Chat Component</div>;
 };
 
