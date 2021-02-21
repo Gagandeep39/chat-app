11d8f4e85a8e11e69698e39938c4581c342f895e
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-21 08:02:12 +0000

    Fixed error username taken

--- server/users.js
+++ server/users.js
@@ -10,9 +10,8 @@ const users = [];
 const addUser = ({ id, name, room }) => {
   name = name.trim().toLowerCase();
   room = room.trim().toLowerCase();
-
   const existingUser = users.find(
-    (user) => user.room === room && user.name === user.name
+    (user) => user.room === room && user.name === name
   );
   if (existingUser) return { error: 'Username is taken' };
   const user = { id, name, room };
