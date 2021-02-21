863ff32ed5f8f1dbd698cceb20f8a370ca032530
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-21 09:01:05 +0000

    Added cors

--- server/index.js
+++ server/index.js
@@ -6,13 +6,14 @@
  * @desc [description]
  */
 const express = require('express');
-
+const cors = require('cors');
 const http = require('http');
 const router = require('./router');
 const { addUser, getUser, removeUser, getUsersInRoom } = require('./users');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
 
 const server = http.createServer(app);
 
