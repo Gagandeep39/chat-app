e14d15825f8858a826bb4a58ba9a04a7ee0abb8e
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-21 09:05:55 +0000

    Switched cors location

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ const { addUser, getUser, removeUser, getUsersInRoom } = require('./users');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
 
 const server = http.createServer(app);
 
@@ -74,4 +73,5 @@ socketio.on('connection', (socket) => {
 });
 
 app.use(router);
+app.use(cors());
 server.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
